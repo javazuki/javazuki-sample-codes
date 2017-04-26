99995bf5d998b6caa3c70a91a58325292ca9e1aa
富岡隆幸 <ttomioka@tmkworks.jp>
2017-04-26 13:53:02 +0900

    start

---
+++ javazuki-sample-codes-javase/src/main/java/com/javazuki/samples/javase/array/ArraySample.java
@@ -0,0 +1,13 @@
+package com.javazuki.samples.javase.array;
+
+public class ArraySample {
+
+  public static void main(String[] args) {
+    int[] scores = { 30, 80, 40 };
+
+    System.out.println("scores[0]:" + scores[0]);
+    System.out.println("scores[1]:" + scores[1]);
+    System.out.println("scores[2]:" + scores[2]);
+  }
+
+}
